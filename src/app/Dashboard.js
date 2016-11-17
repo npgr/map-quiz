9a14d37f0a6c5d8bcee54a2e054746777441be44
Nuno <e_npgr@yahoo.com>
2016-11-17 10:41:45 -0400

    Add Navbar with bootstrap + some style with cerulean Theme

--- src/app/Dashboard.js
+++ src/app/Dashboard.js
@@ -1,11 +1,10 @@
 import React from "react";
-import { Glyphicon, FormGroup, FormControl, ControlLabel, Panel, Jumbotron, Grid, Row, Col } from "react-bootstrap";
+import { Glyphicon, FormGroup, FormControl, ControlLabel, Panel, Grid, Row, Col } from "react-bootstrap";
 
 export default class Dashboard extends React.Component {
 
 	/*constructor(props) {
 		super(props)
-		console.log('About')
 	}*/
 	 
   render() {
@@ -14,7 +13,7 @@ export default class Dashboard extends React.Component {
 		<Grid>
 		  <Row>
 		    <Col xs={6} sm={6} md={6} lg={6}>
-			  <Panel header="Type">
+			  <Panel bsStyle="success" header={<div><Glyphicon glyph="star"/> MAP</div>}>
 		      <FormGroup controlId="formControlsSelect1">
 				<ControlLabel>Select</ControlLabel>
 				<FormControl componentClass="select" placeholder="Value 2">
@@ -25,7 +24,7 @@ export default class Dashboard extends React.Component {
 			  </Panel>
 			</Col>
 			<Col xs={6} sm={6} md={6} lg={6}>
-		      <Panel header="Data">
+		      <Panel bsStyle="primary" header="Data">
 				<FormGroup controlId="formControlsSelect2">
 				<FormControl componentClass="select">
 					<option value="select">Value 1</option>
@@ -35,9 +34,9 @@ export default class Dashboard extends React.Component {
 		      </Panel>
 			</Col>
 		  </Row>
-		  <Row className="show-grid">
+		  <Row>
 		    <Col>
-		      <Panel header={<div><Glyphicon glyph="star"/> MAP</div>}>
+		      <Panel>
 				<Glyphicon glyph="star" /> MAP
 		      </Panel>
 			</Col>
--- src/app/Layout.js
+++ src/app/Layout.js
@@ -1,46 +1,62 @@
 import React from 'react';
-import { Link } from 'react-router';
-
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import Appbar from 'material-ui/Appbar';
+//import { Link } from 'react-router';
 
 import Menu from './Menu';
 
+import { Navbar, Nav, NavItem, NavDropdown, MenuItem } from 'react-bootstrap';
+
 export default class Layout extends React.Component {
 	
-	constructor(props) {
+	/*constructor(props) {
 		super(props)
-		this.state = {
-			menuIsOpen: false
-		}
+		
 		this.closeMenu = this.closeMenu.bind(this)
-		this.openMenu = this.openMenu.bind(this)
+	}*/
+	
+	goPage = this.goPage.bind(this)
+ 
+	goPage(e) {
+		let route = e.target.id
+		this.props.history.push(route)
+		//this.context.router.push(route)
 	}
 	
-	openMenu = () => this.setState({menuIsOpen: true})
-
-	closeMenu = () => this.setState({menuIsOpen: false})
-	
 	render() {
-	 const { closeMenu, openMenu } = this
-	 
-	 const topLine = { height: "7px", backgroundColor: "#34829c"}
-	 let img = { maxWidth: "100%", height: "auto" }
-	 let line = { display:"flex", backgroundColor:"#9ccc66", height:"5px" }
-	 let barStyle = {backgroundColor: '#3a80ab', fontSize: '16px', fontWeight: '200'}
-	 	 
-	 return(
-	  <MuiThemeProvider>
-	   <div>
-		<Appbar title="Map Stats" style={barStyle}
-			onLeftIconButtonTouchTap={openMenu}/>
-		<div style={topLine} />
-			<Menu closeMenu={closeMenu} menuIsOpen={this.state.menuIsOpen}
-				history={this.props.history}/>
-			{this.props.children}
-	   </div>
-	  </MuiThemeProvider>
-	)
+		const { goPage } = this
+		
+		const AppNavbar = (
+		  <Navbar inverse collapseOnSelect>
+			<Navbar.Header>
+			  <Navbar.Brand>
+				<div onClick={goPage} id="/">MAP Stats</div>
+			  </Navbar.Brand>
+			  <Navbar.Toggle />
+			</Navbar.Header>
+			<Navbar.Collapse>
+			  <Nav>
+				<NavItem eventKey={1} href="#">Link</NavItem>
+				<NavItem eventKey={2} href="#">Link</NavItem>
+				<NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
+				  <MenuItem eventKey={3.1}>Action</MenuItem>
+				  <MenuItem eventKey={3.2}>Another action</MenuItem>
+				  <MenuItem eventKey={3.3}>Something else here</MenuItem>
+				  <MenuItem divider />
+				  <MenuItem eventKey={3.3}>Separated link</MenuItem>
+				</NavDropdown>
+			  </Nav>
+			  <Nav pullRight>
+				<NavItem eventKey={1} onClick={goPage} id="about">About</NavItem>
+				<NavItem eventKey={2} href="#">Link Right</NavItem>
+			  </Nav>
+			</Navbar.Collapse>
+		  </Navbar>
+		);
+		return(
+			<div>
+				{AppNavbar}
+				{this.props.children}
+			</div>
+		)
   }
 }
 
