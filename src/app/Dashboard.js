c0ee2eb1ca5cebe996125541f9794d10cc1f8b56
Nuno <e_npgr@yahoo.com>
2016-11-24 22:18:58 -0400

    Dashboard.js: import & use Map & StateStore

--- src/app/Dashboard.js
+++ src/app/Dashboard.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { Form, Glyphicon, FormGroup, FormControl, ControlLabel, Panel, Grid, Row, Col } from "react-bootstrap";
+import store from "./store/StateStore"
+import Map from "./Map.js"
 
 export default class Dashboard extends React.Component {
 
@@ -14,111 +16,42 @@ export default class Dashboard extends React.Component {
 		this.panel1Toggle = this.panel1Toggle.bind(this)
 		this.panel2Toggle = this.panel2Toggle.bind(this)
 		setTimeout(function() {
-			this.load_map('world') }.bind(this)
+			//this.load_map('world') }.bind(this)
+			Map.load_map('world') }.bind(this)
 		  , 1000)
+		  
+		/*setTimeout(function() {
+			store.states.forEach(function(el) {
+				console.log('dashboard load: ',el)
+			})
+		}.bind(this), 2000)*/
 	}
 	
 	panel1Toggle = () => this.setState({panel1Open: !this.state.panel1Open})
 	
 	panel2Toggle = () => this.setState({panel2Open: !this.state.panel2Open})
 	
-	load_world = () => this.load_map('world')
-	
-	load_map(map) {
-		document.getElementById('theMap').innerHTML = ''
-		new Datamap({
-            //scope: 'world',
-			//scope: 'usa',
-			scope: map,
-			projection: 'mercator',
-            element: document.getElementById('theMap'),
-			// Zoom 
-			/*setProjection: function(element) {
-				var projection = d3.geo.equirectangular()
-					//Africa
-					//.center([23, 2])
-					//.rotate([4.4, 0])
-					//.scale(300)
-					// Europa
-					.center([23, 52])
-					.rotate([4.4, 0])
-					.scale(600)
-					.translate([element.offsetWidth / 2, element.offsetHeight / 2]);
-					var path = d3.geo.path()
-					.projection(projection);
-
-				return {path: path, projection: projection};
-			},*/
-			height: 420,
-			geographyConfig: {
-              borderColor: '#444',
-              borderWidth: 0.5,
-			  popupTemplate: function(geography, data) {
-				//Load some data
-				window.country = geography.properties.iso
-				return '<div class="hoverinfo">Country: ' + geography.properties.name 
-				//+ '<br>Population: '+data.population+'</div>'
-			  },
-            },
-			fills: {
-              'Red': 'red',
-			  'defaultFill': '#dddddd'
-			  
-			},
-			data:{
-				"USA": {
-					"fillKey": "Red",
-					"population": 1000
-				}
-			}
-		})
-	}
-	
-	loadx_map () {
-		var election = new Datamap({
-            scope: 'usa',
-            element: document.getElementById('theMap'),
-			height: 520,
-            geographyConfig: {
-              borderColor: '#444',
-              borderWidth: 0.5
-            },
-            bubblesConfig: {
-			        //animate: false,
-              borderColor: '#000',
-              borderWidth: 1,
-              fillOpacity: 1.0,
-              filterKey: 'dropShadow',
-              popupTemplate: function(geography, data) {
-                return '<div class="hoverinfo">Some From New: data about ' + data.centered + '</div>'
-              }
-            },
-            fills: {
-              //'defaultFill': '#dddddd',
-			  'defaultFill': '#e99b63',
-              'good': 'url(#good)',
-              'medium': 'url(#medium)',
-              'bad': 'url(#bad)',
-              'horizontalStripe': 'url(#horizontal-stripe)',
-              'diagonalStripe': 'url(#diagonal-stripe)'
-            },
-            filters: {
-              'dropShadow': 'url(#dropShadow)',
-              'bigShadow': 'url(#bigShadow)'
-            },
-            data:{
-              'TX': {fillKey: 'diagonalStripe'}
-            }
-          });
-	}
+	//load_world = () => this.load_map('world')
 	
 	change_map(e) {
-		//console.log()
-		this.load_map(e.target.value)
+		Map.load_map(e.target.value)
 	}
 	
 	msg() {
+		var obj = {}
+		obj[window.country.iso] = 'green'
+		obj.FRA = 'green'
+		//obj[window.country.iso] = {fillKey: 'Red'}
+		//this.map.updateChoropleth(obj);
+		Map.updateChoropleth(obj);
 		console.log(window.country)
+		
+		setTimeout(function() {
+			var obj = {}
+			obj.FRA = {fillKey: 'defaultFill'}
+			Map.updateChoropleth(obj); 
+			 }.bind(this)
+		  , 2500)
 	}
 	
 	render() {
