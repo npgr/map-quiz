4ea94cace971c5daa69af3807fa73dcabba715c9
Nuno <e_npgr@yahoo.com>
2016-12-01 23:15:57 -0400

    Show/Hide State names on mouse hover + UI improvement + Incorrect List

--- src/app/Dashboard.js
+++ src/app/Dashboard.js
@@ -11,15 +11,18 @@ export default class Dashboard extends React.Component {
 		super(props)
 		this.state = {
 			panel1Open: true,
-			panel2Open: true
+			panel2Open: true,
+			incorrectList: []
 		}
-		
+		this.stateAnswer = ''
+		this.map = 'usa'
+		this.showPopUpA = true
 		this.panel1Toggle = this.panel1Toggle.bind(this)
 		this.panel2Toggle = this.panel2Toggle.bind(this)
-		store.load_from_url('usa')
+		//store.load_from_url('usa')
 		setTimeout(function() {
 			//this.load_map('world') }.bind(this)
-			Map.load_map('usa') }.bind(this)
+			Map.load_map(this.map, this.showPopUp) }.bind(this)
 		  , 1000)
 		  
 		/*setTimeout(function() {
@@ -34,24 +37,43 @@ export default class Dashboard extends React.Component {
 	panel2Toggle = () => this.setState({panel2Open: !this.state.panel2Open})
 	
 	change_map(e) {
-		Map.load_map(e.target.value)
+		this.map = e.target.value
+		Map.load_map(this.map, this.showPopUpA)
 	}
 	
 	msg() {
 		store.result.questions++
-		//console.log('answer: ', store.answer(), ' ,click: ', window.country.name) 
+		//console.log('answer: ', store.answer(), ' ,click: ', window.country) 
 		if (store.answer().name == window.country.name)
 		{
 			store.result.rigth++
-			alert('Correct !!!')
+			var obj = {}
+			obj[store.answer().code] = 'green'
+			Map.updateChoropleth(obj)
+			store.next()	
+			//alert('Correct !!!')
 		}
-			
 		 else
 		{
 			store.result.wrong++
-			alert('Incorrect !!!') 
+			var obj = {}
+			obj[store.answer().code] = 'green'
+			Map.updateChoropleth(obj)
+			var incorrectList = this.state.incorrectList
+			incorrectList.unshift({
+					code: store.answer().code,
+					name: store.answer().name
+			})
+			this.setState({ incorrectList: incorrectList });
+			setTimeout(function() {
+				var obj = {}
+				obj[store.answer().code] = {fillKey: 'defaultFill'}
+				Map.updateChoropleth(obj); 
+				store.next()
+			 }.bind(this)
+		    , 1500)
+			//alert('Incorrect !!!') 
 		}
-		store.next()	
 		/*var obj = {}
 		obj[window.country.iso] = 'green'
 		obj.FRA = 'green'
@@ -80,6 +102,11 @@ export default class Dashboard extends React.Component {
 		obj[store.answer().code] = 'green'
 		//obj[window.country.iso] = {fillKey: 'Red'}
 		//this.map.updateChoropleth(obj);
+		var incorrectList = this.state.incorrectList
+		incorrectList.unshift({
+					code: store.answer().code,
+					name: store.answer().name
+		})
 		Map.updateChoropleth(obj);
 		//console.log(window.country)
 		
@@ -92,26 +119,54 @@ export default class Dashboard extends React.Component {
 		  , 2000)
 	}
 	
+	showPopUp(e) {
+		if (e.target.value == 'yes')
+			this.showPopUpA = true
+		 else
+			this.showPopUpA = false
+		Map.load_map(this.map, this.showPopUpA)
+	}
+	
+	showAnswer(e) {
+		this.stateAnswer = e.target.id
+		var obj = {}
+		obj[this.stateAnswer] = 'green'
+		Map.updateChoropleth(obj);
+		setTimeout(function(e) {
+			var obj = {}
+			obj[this.stateAnswer] = {fillKey: 'defaultFill'}
+			Map.updateChoropleth(obj); 
+			}.bind(this)
+		  , 1500)
+	}
+	
 	render() {
 		const { panel1Toggle, panel2Toggle } = this
 		const { panel1Open, panel2Open } = this.state
 		
 		const panel1Header = (
-			 <Form horizontal>
-				  <FormGroup controlId="mapSelected">
-					<Col componentClass={ControlLabel} sm={1} md={1} lg={1}>
-						Map
-					</Col>
-					<Col sm={4} md={4} lg={3}>
-					  <FormControl onChange={this.change_map.bind(this)} componentClass="select">
-						<option value="usa">USA</option>
-						<option value="world">World</option>
-					  </FormControl>
-					</Col>
-				  </FormGroup>
-			 </Form>
+			<Row> 
+				<Col sm={1} md={1} lg={1} style={{marginTop: "10px", marginLeft: "25px"}} componentClass={ControlLabel}>
+					Map
+				</Col>
+				<Col sm={2} md={2} lg={2}>
+				  <FormControl onChange={this.change_map.bind(this)} componentClass="select">
+					<option value="usa">USA</option>
+					<option value="world">World</option>
+				  </FormControl>
+				</Col>
+				<Col sm={2} md={2} lg={2} style={{marginTop: "10px", marginLeft: "25px"}} componentClass={ControlLabel}>
+					Show names
+				</Col>
+				<Col sm={2} md={2} lg={2}>
+				  <FormControl onChange={this.showPopUp.bind(this)} componentClass="select">
+					<option value="yes">Yes</option>
+					<option value="no">No</option>
+				  </FormControl>
+				</Col>
+			</Row>
 		)
-		
+			
 		/*const panel1Header = (
 			<div onClick={panel1Toggle}>Question</div>
 		)*/
@@ -125,27 +180,41 @@ export default class Dashboard extends React.Component {
 			bottom = (<Button bsStyle="success" onClick={this.next.bind(this)}>Start</Button>)
 		 else if (store.quiz.question != 'End of Quiz')
 			bottom = (<Button bsStyle="success" onClick={this.answer.bind(this)}>Get Answer</Button>)
-		
+
+		var incorrectList = []
+		this.state.incorrectList.forEach(function(el) {
+			incorrectList.push(
+				<div id={el.code}> 
+					{el.name}
+				</div>)
+		})
 	  return (
 		<Grid>
 		  <Row>
-		    <Col sm={9} md={9} lg={9}>
+		    <Col>
 			  <Panel header={panel1Header} bsStyle="success" style={{backgroundColor: "#d8ecf3"}}>
+				  <Col style={{padding:"5px"}} sm={9} md={9} lg={9}>
 					<div id="theMap" onClick={this.msg.bind(this)}>Loading Map...</div>
-		      </Panel>
-			</Col>
-		    <Col sm={3} md={3} lg={3}>
-			  <Panel collapsible expanded={panel1Open} header="Question" bsStyle="success">
-				  <div>{store.quiz.question}</div>
-				  <div>{store.quiz.answer}</div>
-				  <div style={{marginTop: '10px'}}>{bottom}</div>
+				  </Col>
+				  <Col style={{padding:"0"}} sm={3} md={3} lg={3}>
+					<Panel header="Question" collapsible expanded={panel1Open} bsStyle="success">
+						<div>{store.quiz.question}</div>
+						<div>{store.quiz.answer}</div>
+						<div style={{marginTop: '10px'}}>{bottom}</div>
+					</Panel>
+					<Panel style={{padding:"0"}} collapsible expanded={panel2Open} header={panel2Header} bsStyle="primary">
+						<div>Questions: {store.result.questions}</div>
+						<div>Correct: {store.result.rigth}</div>
+						<div>Incorrect: {store.result.wrong}</div>
+					</Panel>
+					<Panel header="Incorrect Answers" bsStyle="warning"
+						style={{height:"140px", overflowY:"scroll"}} onClick={this.showAnswer.bind(this)}>
+							{incorrectList}
+					</Panel>
+				  </Col>
 			  </Panel>
-			  <Panel collapsible expanded={panel2Open} header={panel2Header} bsStyle="primary">
-				<div>Questions: {store.result.questions}</div>
-				<div>Rigth: {store.result.rigth}</div>
-				<div>Wrong: {store.result.wrong}</div>
-		      </Panel>
 			</Col>
+		    
 		  </Row> 
 		</Grid>
 	  )
--- src/app/Map.js
+++ src/app/Map.js
@@ -4,7 +4,18 @@
 
 class MapClass {
 
-	load_map(map) {
+
+	/*load_map(map) {
+		var my_awesome_script = document.createElement('script');
+		my_awesome_script.setAttribute('src','assets/maps/datamaps.all.hires.min.js');
+		var x = document.getElementById('xx')
+		x.appendChild(my_awesome_script)
+		//document.body.appendChild(my_awesome_script);
+		my_awesome_script.onload = this.load_map2(map);
+		//this.load_map2(map)
+	}*/
+	
+	load_map(map, showPopUp) {
 		document.getElementById('theMap').innerHTML = ''
 		this.map = new Datamap({
             //scope: 'world',
@@ -33,10 +44,12 @@ class MapClass {
 			geographyConfig: {
               borderColor: '#444',
               borderWidth: 0.5,
+			  //popupOnHover: showPopUp,
 			  popupTemplate: function(geography, data) {
 				//Load some data
 				window.country = geography.properties
-				return '<div class="hoverinfo">' + geography.properties.name 
+				if (showPopUp)
+					return '<div class="hoverinfo">' + geography.properties.name 
 				//+ '<br>Population: '+data.population+'</div>'
 			  },
             },
